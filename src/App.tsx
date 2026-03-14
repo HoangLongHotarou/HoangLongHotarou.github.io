6c20cebaf527ffd1bfc91a31cd2289ce8510477c
unknown <longhoangdazai@gmail.com>
2026-03-14 22:41:02 +0700

    update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import AppShell from './layouts/AppShell';
 import Home from './pages/Home/Home';
